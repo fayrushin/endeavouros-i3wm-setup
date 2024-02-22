def8a3d05ad17f06415851ee18566937c06ec580
Ravil Fayrushin <ravil.fayrushin@gmail.com>
2024-02-22 09:38:42 +0300

    refactor toggle_dark_light_mode

--- .local/bin/toggle_dark_light_mode.sh
+++ .local/bin/toggle_dark_light_mode.sh
@@ -6,15 +6,12 @@ if [[ "$#" -ne 2 ]]; then
 fi
 mode=$1
 user=$2
-n=$(grep -n 'colors: ' "/home/${user}/.config/alacritty/alacritty.yml" | cut -d ':' -f1)
-sed -i "${n}s/.*/colors: \*${mode}/" "/home/${user}/.config/alacritty/alacritty.yml"
-
+cp "/home/${user}/.config/tmux/tmux.${mode}.conf" "/home/${user}/.tmux.conf"
+cp "/home/${user}/.config/alacritty/alacritty.${mode}.toml" "/home/${user}/.config/alacritty/alacritty.toml"
 n1=$(grep -n 'colorscheme =' "/home/${user}/.config/nvim/lua/user/init.lua" | cut -d ':' -f1)
+scheme="nightfox"
 if [[ "$mode" = "light" ]]; then
-	sed -i "${n1}s/.*/  colorscheme = \"dayfox\",/" "/home/${user}/.config/nvim/lua/user/init.lua"
-	cp "/home/${user}/.config/tmux/tmux.light.conf" "/home/${user}/.tmux.conf"
-else
-	sed -i "${n1}s/.*/  colorscheme = \"nightfox\",/" "/home/${user}/.config/nvim/lua/user/init.lua"
-	cp "/home/${user}/.config/tmux/tmux.dark.conf" "/home/${user}/.tmux.conf"
+	scheme="dayfox"
 fi
+sed -i "${n1}s/.*/  colorscheme = \"${scheme}\",/" "/home/${user}/.config/nvim/lua/user/init.lua"
 tmux source-file "/home/${user}/.tmux.conf"
