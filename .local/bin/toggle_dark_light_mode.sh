264799f4ed08608ddd2981b2c606ab94f53697cc
Файрушин Равил <r.fajrushin@dssl.ru>
2024-03-20 19:28:44 +0300

    toggle system color

--- .local/bin/toggle_dark_light_mode.sh
+++ .local/bin/toggle_dark_light_mode.sh
@@ -8,6 +8,8 @@ mode=$1
 user=$2
 cp "/home/${user}/.config/tmux/tmux.${mode}.conf" "/home/${user}/.tmux.conf"
 cp "/home/${user}/.config/alacritty/alacritty.${mode}.toml" "/home/${user}/.config/alacritty/alacritty.toml"
+cp "/home/${user}/.config/gtk-3.0/settings.${mode}.ini" "/home/${user}/.config/gtk-3.0/settings.ini"
+cp "/home/${user}/.Xresources.${mode}" "/home/${user}/.Xresources"
 n1=$(grep -n 'colorscheme =' "/home/${user}/.config/nvim/lua/user/init.lua" | cut -d ':' -f1)
 scheme="nightfox"
 if [[ "$mode" = "light" ]]; then
