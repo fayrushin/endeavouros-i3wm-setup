0b2ad5df35c9106937d68bd75bca9ddc9b5825f2
Файрушин Равил <r.fajrushin@dssl.ru>
2024-04-02 17:38:05 +0300

    up to v4

--- .local/bin/toggle_dark_light_mode.sh
+++ .local/bin/toggle_dark_light_mode.sh
@@ -16,10 +16,10 @@ cp "/home/${user}/.config/tmux/tmux.${mode}.conf" "/home/${user}/.tmux.conf"
 cp "/home/${user}/.config/alacritty/alacritty.${mode}.toml" "/home/${user}/.config/alacritty/alacritty.toml"
 cp "/home/${user}/.config/gtk-3.0/settings.${mode}.ini" "/home/${user}/.config/gtk-3.0/settings.ini"
 cp "/home/${user}/.Xresources.${mode}" "/home/${user}/.Xresources"
-n1=$(grep -n 'colorscheme =' "/home/${user}/.config/nvim/lua/user/init.lua" | cut -d ':' -f1)
+n1=$(grep -n 'colorscheme =' "/home/${user}/.config/nvim/lua/plugins/astroui.lua" | cut -d ':' -f1)
 scheme="nightfox"
 if [[ "$mode" = "light" ]]; then
 	scheme="dayfox"
 fi
-sed -i "${n1}s/.*/  colorscheme = \"${scheme}\",/" "/home/${user}/.config/nvim/lua/user/init.lua"
+sed -i "${n1}s/.*/    colorscheme = \"${scheme}\",/" "/home/${user}/.config/nvim/lua/plugins/astroui.lua"
 # tmux source-file "/home/${user}/.tmux.conf"
