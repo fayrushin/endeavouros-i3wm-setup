07ccf6c32d247adbad566308fcdc5af57440ce25
Файрушин Равил <r.fajrushin@dssl.ru>
2024-07-25 20:07:05 +0300

    use external display

--- .local/bin/monitor.sh
+++ .local/bin/monitor.sh
@@ -3,8 +3,8 @@
 hdmi=$(xrandr -q | grep -w "connected" | grep -v eDP | awk '{print $1}')
 if [ "$hdmi" ]; then
 	edp=$(xrandr -q | grep -w "connected" | grep -v "${hdmi}" | awk '{print $1}')
-	xrandr --output "$hdmi" --primary --mode 3840x2160 --pos 2560x0 --rotate normal \
-		--output "$edp" --mode 2560x1600 --pos 0x560 --rotate normal
+	xrandr --output "$hdmi" --primary --mode 2560x1440 --pos 2560x0 --rate 144 --rotate normal \
+		--output "$edp" --mode 2560x1600 --pos 0x0 --rotate normal
 else
 	edp=$(xrandr -q | grep -w "connected" | awk '{print $1}')
 	xrandr --output $edp --primary --mode 2560x1600 --rate 240
